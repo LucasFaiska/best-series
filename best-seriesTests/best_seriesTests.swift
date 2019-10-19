8ca240db8c93fb740aa7ef9ce00e09dcfd5952a5
Lucas de Oliveira <lucas.faiskaa@gmail.com>
2019-10-19 01:35:40 -0300

    Initial Commit

---
+++ best-seriesTests/best_seriesTests.swift
@@ -0,0 +1,34 @@
+//
+//  best_seriesTests.swift
+//  best-seriesTests
+//
+//  Created by Lucas de Oliveira on 10/19/19.
+//  Copyright © 2019 Lucas de Oliveira. All rights reserved.
+//
+
+import XCTest
+@testable import best_series
+
+class best_seriesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
