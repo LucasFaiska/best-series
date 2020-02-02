5eb4d16dd5f4f50aaf0c988b4711ff87c78eaa6f
Lucas de Oliveira <lucas.faiskaa@gmail.com>
2020-02-02 02:12:19 -0300

    Moving poster and gradient to inside CardView class. Fix poster image aspect ratio.

--- best-series/Presentation/Components/ListItemCardView.swift
+++ best-series/Presentation/Components/ListItemCardView.swift
@@ -8,8 +8,9 @@
 
 import Foundation
 import UIKit
+import Kingfisher
 
-class CardView: UIView {
+class ListItemCardView: UIView {
     
     var cornerRadius: CGFloat {
         get {
@@ -21,7 +22,7 @@ class CardView: UIView {
             layer.masksToBounds = false
         }
     }
-
+    
     var shadowOpacity: Float {
         get {
             return layer.shadowOpacity
@@ -31,7 +32,7 @@ class CardView: UIView {
             layer.shadowColor = UIColor.darkGray.cgColor
         }
     }
-
+    
     var shadowOffset: CGSize {
         get {
             return layer.shadowOffset
@@ -43,16 +44,36 @@ class CardView: UIView {
         }
     }
     
-    override func draw(_ rect: CGRect) {
-        super.draw(rect)
+    var posterUrl: URL? = nil
+    
+    private func drawGradient() {
         let gradient = CAGradientLayer()
         gradient.startPoint = CGPoint(x: 0, y: 0.6)
         gradient.endPoint = CGPoint(x: 0, y: 1)
         gradient.frame = self.bounds
         gradient.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
         gradient.locations = [0.0, 0.5]
-        gradient.opacity = 0.9
+        gradient.opacity = 0.7
         gradient.cornerRadius = self.cornerRadius
         self.layer.insertSublayer(gradient, at: 10)
+    }
+    
+    private func drawPoster() {
+        let posterImageView = UIImageView(frame: .zero)
+        posterImageView.contentMode = .scaleAspectFill
+        posterImageView.translatesAutoresizingMaskIntoConstraints = false
+        posterImageView.frame = self.bounds
+        posterImageView.kf.setImage(with: posterUrl)
+        self.addSubview(posterImageView)
+        NSLayoutConstraint.activate([
+            posterImageView.heightAnchor.constraint(equalTo: self.heightAnchor, constant: 0),
+            posterImageView.widthAnchor.constraint(equalTo: self.widthAnchor, constant: 0),
+        ])
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        drawPoster()
+        drawGradient()
     }
 }
--- best-series/Presentation/Serie/SerieList/uiKit/SerieListItemCell.swift
+++ best-series/Presentation/Serie/SerieList/uiKit/SerieListItemCell.swift
@@ -7,41 +7,33 @@
 //
 
 import UIKit
-import Kingfisher
 
 class SerieListItemCell: UITableViewCell {
     static let reuseIdentifier = "SerieCell"
     
     var serie: Serie? {
         didSet {
-            serieTitleLabel.text = serie?.title
-            seriePosterImageView.kf.setImage(with: serie?.posterUrl)
+            serieCellCardView.posterUrl = serie?.posterUrl
         }
     }
     
-    private let serieCellCardView: CardView = {
-        let cardView = CardView()
+    private let serieCellCardView: ListItemCardView = {
+        let cardView = ListItemCardView()
         cardView.cornerRadius = 10
         cardView.shadowOpacity = 1
         cardView.shadowOffset = CGSize(width: 1, height: 1)
         cardView.translatesAutoresizingMaskIntoConstraints = false
+        cardView.backgroundColor = UIColor.white
+        //@TODO this clipsToBounds remove the shadows from card, fix it soon
         cardView.clipsToBounds = true
         return cardView
     }()
     
     private let serieTitleLabel = UILabel.with(textColor: .black, fontSize: 17)
-        
-    private let seriePosterImageView: UIImageView = {
-        let imageView = UIImageView(frame: .zero)
-        imageView.contentMode = .scaleToFill
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
-    }()
     
-    //@TODO fazer uma classe cardview que tenha uma UiImageView e uma UiLabel e o gradient por cima
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        addCellContent(serieCellCardView, seriePosterImageView)
+        addCellContent(serieCellCardView)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -53,21 +45,14 @@ class SerieListItemCell: UITableViewCell {
         self.serie = nil
     }
     
-    private func addCellContent(_ cardView: CardView, _ posterImageView: UIImageView) {
+    private func addCellContent(_ cardView: ListItemCardView) {
         addSubview(cardView)
-        cardView.addSubview(posterImageView)
-        //posterImageView.frame = cardView.bounds
-        
         NSLayoutConstraint.activate([
             cardView.heightAnchor.constraint(equalToConstant: 500),
             cardView.leftAnchor.constraint(equalTo: leftAnchor, constant: 20),
             cardView.rightAnchor.constraint(equalTo: rightAnchor, constant: -20),
             cardView.topAnchor.constraint(equalTo: topAnchor, constant: 20),
-            cardView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20),
-            posterImageView.heightAnchor.constraint(equalTo: cardView.heightAnchor, constant: 0),
-            posterImageView.widthAnchor.constraint(equalTo: cardView.widthAnchor, constant: 0),
-            posterImageView.leftAnchor.constraint(equalTo: leftAnchor, constant: 20),
-            posterImageView.rightAnchor.constraint(equalTo: rightAnchor, constant: -20),
+            cardView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20)
         ])
     }
 }
